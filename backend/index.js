2f961c68e76e2b587d41eaa54911baef3c8c2c2b
Pavan Raj <pvnnraj@gmail.com>
2026-01-17 14:02:55 +0530

    setup cors and hit /health from frontend

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,11 @@
 import express from 'express'
+import cors from "cors";
 
 const app = express();
-
+app.use(express.json())
+app.use(cors({
+  origin: "http://localhost:5173",
+}))
 const PORT = process.env.PORT || 5000;
 
 app.get('/api/health', (request, response)=>{
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,33 +1,27 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useEffect, useState } from 'react'
+import axios from "axios"
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [status, setStatus] = useState("");
+
+  useEffect(() => {
+    const checkHealth = async () => {
+      try {
+        await axios.get("http://127.0.0.1:5000/api/health");
+        setStatus("healthy");
+      } catch (err) {
+        console.error(err);
+        setStatus("unreachable");
+      }
+    }
+    checkHealth();
+  }, [])
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <p>Welcome to TaskB app</p>
+      <p>Backend is {status}</p>
     </>
   )
 }

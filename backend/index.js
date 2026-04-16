be9cff2d09da39a7990094334322120748bd0af5
Pavan Raj <pvnnraj@gmail.com>
2026-04-16 17:21:38 +0530

    changed port number for local dev

--- backend/index.js
+++ backend/index.js
@@ -26,7 +26,6 @@ app.get("/api/health", (request, response) => {
 
 // Only listen locally
 if (process.env.NODE_ENV !== "production") {
-  const PORT = process.env.PORT || 3000;
   app.listen(PORT, () => console.log(`Server listeniing on port ${PORT}`));
 }
 
